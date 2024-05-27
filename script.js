420a888dd0797b0a60b0e11d0aa9ef440250def1
Prathu Dwivedi <prathudwivedi241@gmail.com>
2024-05-27 21:46:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("wheel", function(dets) {
+    if (dets.deltaY > 0) {
+        gsap.to(".marque", {
+            xPercent: -200,
+            repeat: -1,
+            duration: 3,
+            ease: "none"
+        });
+        gsap.to(".marque img", {
+            rotate: 180
+        });
+    } else {
+        gsap.to(".marque", {
+            xPercent: 0,
+            repeat: -1,
+            duration: 4,
+            ease: "none"
+        });
+        gsap.to(".marque img", {
+            rotate: 0
+        });
+    }
+});
+
